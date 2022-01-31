e0f331a45ebc0db8543bc544b13ab18e52ee4d74
Hexadecane <36948730+Hexadecane@users.noreply.github.com>
2022-01-31 15:01:22 -0500

    Change filesystem code to C++17 standard

--- src/util.h
+++ src/util.h
@@ -5,7 +5,8 @@
 #include <iostream>
 #include <string>
 #include <utility>
-#include <experimental/filesystem>  // This needs to be used instead of <filesystem> to compile under GCC!
+#include <filesystem>  
+// ^^^ This might need to be <experimental/filesystem> to compile under GCC, depending on the GCC version used!
 #include <unordered_map>
 #include <random>
 #include <vector>
@@ -17,7 +18,8 @@ void ThrowError(std::string msg);
 // Global Variables:
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
-namespace FS = std::experimental::filesystem;  // This needs to be std::experimental::filesystem when compiling under GCC!
+namespace FS = std::filesystem;  
+// ^^^ This might need to be std::experimental::filesystem to compile under GCC, depending on the GCC version used!
 
 struct IntVector2 {
 	IntVector2() : _x(0), _y(0) {}
